ca7c28cf8cee66ea2adcba8b5418424c49468e6f
Michael Stewart <stewarmc@jmu.edu>
2022-11-07 13:06:43 -0500

    cheating. giving students part of the part 1 answer like a chump

--- wordInfo.js
+++ wordInfo.js
@@ -7,3 +7,28 @@ variableSizeResults.forEach((result) => {
 });
 
 // assuming you already have rhyme results somewhere, for each of the first 10 results, query the word info api for the rhyming words' info and display them in a dl with that rhyming word
+
+async function begin() {
+  const rhymeResults = await fetch("example-rhyme-results.json");
+  const rhymeResultsJson = await rhymeResults.json();
+  console.log(rhymeResultsJson);
+  const rhymeResultsElems = rhymeResultsJson.map((rhymeWord) => {
+    const resultElem = document.createElement("div");
+    // resultElem.classList.add("result");
+    // if (rhymeWord.score >= 300) {
+    //   resultElem.classList.add("perfect");
+    // } else {
+    //   resultElem.classList.add("perfect");
+    // }
+    // resultElem.dataset.score = rhymeWord.score;
+    resultElem.innerText = rhymeWord.word;
+    return resultElem;
+  });
+  const resultsContainer = document.getElementById('results');
+  // console.log(Array.from(resultsContainer.childNodes));
+  Array.from(resultsContainer.childNodes).forEach((child) => {
+    child.remove();
+  })
+  resultsContainer.append(...rhymeResultsElems);
+}
+begin();
