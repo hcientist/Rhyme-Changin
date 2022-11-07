7010e1e4409feb2511b90601e518c82bfa132ad8
Michael Stewart <stewarmc@jmu.edu>
2022-11-07 14:32:15 -0500

    how more generalized append

--- wordInfo.js
+++ wordInfo.js
@@ -1,10 +1,12 @@
 const query = "hello";
 
-const variableSizeResults = document.querySelectorAll(".result.imperfect");
-variableSizeResults.forEach((result) => {
-  const resultScore = parseInt(result.dataset.score, 10);
-  result.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
-});
+function sizeTheWords() {
+  const variableSizeResults = document.querySelectorAll(".result.imperfect");
+  variableSizeResults.forEach((result) => {
+    const resultScore = parseInt(result.dataset.score, 10);
+    result.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
+  });
+}
 
 // assuming you already have rhyme results somewhere, for each of the first 10 results, query the word info api for the rhyming words' info and display them in a dl with that rhyming word
 
@@ -14,21 +16,23 @@ async function begin() {
   console.log(rhymeResultsJson);
   const rhymeResultsElems = rhymeResultsJson.map((rhymeWord) => {
     const resultElem = document.createElement("div");
-    // resultElem.classList.add("result");
-    // if (rhymeWord.score >= 300) {
-    //   resultElem.classList.add("perfect");
-    // } else {
-    //   resultElem.classList.add("perfect");
-    // }
-    // resultElem.dataset.score = rhymeWord.score;
-    resultElem.innerText = rhymeWord.word;
+    resultElem.classList.add("result");
+    if (rhymeWord.score >= 300) {
+      resultElem.classList.add("perfect");
+    } else {
+      resultElem.classList.add("imperfect");
+    }
+    resultElem.dataset.score = rhymeWord.score;
+    // resultElem.innerText = rhymeWord.word;
+    resultElem.append(rhymeWord.word);
     return resultElem;
   });
-  const resultsContainer = document.getElementById('results');
+  const resultsContainer = document.getElementById("results");
   // console.log(Array.from(resultsContainer.childNodes));
   Array.from(resultsContainer.childNodes).forEach((child) => {
     child.remove();
-  })
+  });
   resultsContainer.append(...rhymeResultsElems);
+  sizeTheWords();
 }
 begin();
